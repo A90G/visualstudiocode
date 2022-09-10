2e550b322108364abc0b5a1a9caddf2659c6c60b
Araceli Gittardi <araceligittardi@tdf.edu.ar>
2022-09-09 23:20:01 -0300

    clase lucas

---
+++ segundoCuatri/HolaMundo.js
@@ -0,0 +1,4 @@
+let readlineSync = require('readline-sync');
+let alturaPersona = readlineSync.question("Indique la altura de la persona: ");
+let mensaje= alturaPersona;
+console.log(alturaPersona);
