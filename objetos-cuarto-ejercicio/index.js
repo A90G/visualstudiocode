3a54bcfa2e379f23c19321ac3618fa9728960154
Araceli Gittardi <araceligittardi@tdf.edu.ar>
2022-09-27 22:37:02 -0300

    primera instancia ejercicio selección de fútbol

---
+++ objetos-cuarto-ejercicio/index.js
@@ -0,0 +1,95 @@
+var fs = require('fs');
+var Book = /** @class */ (function () {
+    function Book(pTitle, pAuthor, pEditorial, pidentificationCode) {
+        this.title = pTitle;
+        this.author = pAuthor;
+        this.editorial = pEditorial;
+        this.identificationCode = pidentificationCode;
+    }
+    Book.prototype.getTitle = function () {
+        return this.title;
+    };
+    Book.prototype.setTitle = function (name) {
+        this.title = name;
+    };
+    Book.prototype.getAuthor = function () {
+        return this.author;
+    };
+    Book.prototype.setAuthor = function (nameAuthor) {
+        this.author = nameAuthor;
+    };
+    Book.prototype.getEditorial = function () {
+        return this.editorial;
+    };
+    Book.prototype.setEditorial = function (nameEditorial) {
+        this.editorial = nameEditorial;
+    };
+    Book.prototype.getIdentificationCode = function () {
+        return this.identificationCode;
+    };
+    Book.prototype.setIdentificationCode = function (id) {
+        this.identificationCode = id;
+    };
+    return Book;
+}());
+var BookManager = /** @class */ (function () {
+    function BookManager(pList) {
+        this.bookList = pList;
+    }
+    BookManager.prototype.insertBook = function (book) {
+        this.bookList.push(book);
+        console.log("\"Id Book Code: \"".concat(book.getIdentificationCode(), "\" Book title: \"").concat(book.getTitle(), "\" from \"").concat(book.getAuthor(), "\" publish by \"").concat(book.getEditorial()));
+    };
+    BookManager.prototype.consultBook = function (book) {
+        for (var i = 0; i < this.bookList.length; i++) {
+            if (book.getTitle() === this.bookList[i].getTitle()) {
+                return console.log(this.bookList[i]);
+            }
+        }
+    };
+    BookManager.prototype["delete"] = function (book) {
+        for (var i = 0; i < this.bookList.length; i++) {
+            if (book.getTitle() === this.bookList[i].getTitle()) {
+                this.bookList.splice(i, 1);
+                return console.log("Removed ".concat(book.getTitle(), " from the list"));
+            }
+        }
+        return console.log("Error trying to delete ".concat(book.getTitle(), " please contact suport"));
+    };
+    /*esta funcion la considero redundante, porque ya existe un setter para cada variable, pero era parte de la consigna*/
+    BookManager.prototype.modifyData = function (book, newTitle, newAuthor, newEditorial, newIdCode) {
+        /*ver si de esta forma puedo llamarlo al libro y al encontrarlo: modificarlo*/
+        for (var i = 0; i < this.bookList.length; i++) {
+            if (book.getTitle() === this.bookList[i].getTitle()) {
+                this.bookList[i].setTitle(newTitle);
+                this.bookList[i].setAuthor(newAuthor);
+                this.bookList[i].setEditorial(newEditorial);
+                this.bookList[i].setIdentificationCode(newIdCode);
+                return console.log("New Title set: \"".concat(newTitle, "\" from \"").concat(newAuthor, "\" publish by \"").concat(newEditorial, "\" and new Identification Code asign: \"").concat(newIdCode, "\""));
+            }
+        }
+    };
+    return BookManager;
+}());
+var b1 = new Book("Poema de Gilgamesh", "Anonimous", "unknown", 1);
+var b2 = new Book("Libro de Job", "Anonimous", "unknown", 2);
+var b3 = new Book("Las mil y una noches", "Anonimous", "unknown", 3);
+var b4 = new Book("Saga de Njál", "Anonimous", "unknown", 4);
+var b5 = new Book("Todo se desmorona", "Chinua Achebe", "unknown", 5);
+var b6 = new Book("test", "Anonimous", "unknown", 6);
+var bookListTest = [b1, b2, b3, b4, b5];
+/*con qué método yo puedo saber si existe un libro declarado que yo no haya incluído en mi lista simplemente por haberlo olvidado*/
+var bm1 = new BookManager(bookListTest);
+/*
+bm1.delete(b2);
+bm1.delete(b2);
+
+bm1.insertBook(b6);
+console.log(bm1);
+
+bm1.consultBook(b1);
+bm1.modifyData(b3,"blanco","Negro","xx",44);
+console.log(b3); */
+var texto = fs.readFileSync('readme.txt', 'utf8');
+var palabras = texto.split(' ');
+console.log(palabras);
--- objetos-cuarto-ejercicio/index.ts
+++ objetos-cuarto-ejercicio/index.ts
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 class Book {
     private title: string;
     private author: string;
@@ -56,13 +58,66 @@ class BookManager {
         console.log(`"Id Book Code: "${book.getIdentificationCode()}" Book title: "${book.getTitle()}" from "${book.getAuthor()}" publish by "${book.getEditorial()}`);
     }
 
+    public consultBook(book: Book): void {
+        for (let i: number = 0; i < this.bookList.length; i++) {
+            if (book.getTitle() === this.bookList[i].getTitle()) {
+                return console.log(this.bookList[i]);
+            }     
+        }
+    }
+
+    public delete(book: Book): any {
+        for (let i: number = 0; i < this.bookList.length; i++) {
+            if (book.getTitle() === this.bookList[i].getTitle()) {
+                this.bookList.splice(i, 1);
+                return console.log(`Removed ${book.getTitle()} from the list`);
+            } 
+            }
+        return console.log(`Error trying to delete ${book.getTitle()} please contact suport`);
+        }
     
-    public consultBook(: string): void {
-    }
 
-    public delete(: string): void {
-    }
-
-    public modifyData(: string): void {
+/*esta funcion la considero redundante, porque ya existe un setter para cada variable, pero era parte de la consigna*/
+    public modifyData(book: Book, newTitle: string, newAuthor: string, newEditorial: string, newIdCode: number): void {
+/*ver si de esta forma puedo llamarlo al libro y al encontrarlo: modificarlo*/        
+    for (let i: number = 0; i < this.bookList.length; i++) {
+            if (book.getTitle() === this.bookList[i].getTitle()) {
+                this.bookList[i].setTitle(newTitle);
+                this.bookList[i].setAuthor(newAuthor);
+                this.bookList[i].setEditorial(newEditorial);
+                this.bookList[i].setIdentificationCode(newIdCode);
+                return console.log(`New Title set: "${newTitle}" from "${newAuthor}" publish by "${newEditorial}" and new Identification Code asign: "${newIdCode}"`);
+            }     
+        }
     }
 }
+
+let b1 = new Book ("Poema de Gilgamesh", "Anonimous", "unknown", 1);
+let b2 = new Book ("Libro de Job", "Anonimous", "unknown", 2);
+let b3 = new Book ("Las mil y una noches", "Anonimous", "unknown", 3);
+let b4 = new Book ("Saga de Njál", "Anonimous", "unknown", 4);
+let b5 = new Book ("Todo se desmorona", "Chinua Achebe", "unknown", 5);
+let b6 = new Book ("test", "Anonimous", "unknown", 6);
+
+let bookListTest: Book[] = [b1,b2,b3,b4,b5];
+/*con qué método yo puedo saber si existe un libro declarado que yo no haya incluído en mi lista simplemente por haberlo olvidado*/
+let bm1 = new BookManager (bookListTest);
+
+/*
+bm1.delete(b2);
+bm1.delete(b2);
+
+bm1.insertBook(b6);
+console.log(bm1);
+
+bm1.consultBook(b1);
+bm1.modifyData(b3,"blanco","Negro","xx",44);
+console.log(b3); */
+
+
+
+let texto: string = fs.readFileSync('readme.txt', 'utf8');
+
+let palabras: string[] = texto.split(' ');
+
+console.log(palabras);
