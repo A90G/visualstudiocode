5296c95348e91c95e21575a163118042e378e8ae
irinamarzioni <imarzioni@devartis.com>
2022-10-18 00:16:44 -0300

    correccion

--- objetos-quinto-ejercicio/index.ts
+++ objetos-quinto-ejercicio/index.ts
@@ -3,10 +3,10 @@ import { Futbolista } from "./futbolista";
 import { Masajista } from "./masajista";
 import { Entrenador } from "./entrenador";
 
-let jugador1 = new Futbolista("Leonel","Messi",40, 80,"Jugador de Primera");
-let jugador2 = new Futbolista("Cristiano","Ronaldo",41, 80,"Jugador de Primera");
-let jugador3 = new Futbolista("Luis","Suarez",39, 80,"Suplente");
-let jugador4 = new Futbolista("Angel","Di Maria",37, 80,"Jugador de Primera");
+let jugador1 = new Futbolista("Leonel", "Messi", 40, 80, "Jugador de Primera");
+let jugador2 = new Futbolista("Cristiano", "Ronaldo", 41, 80, "Jugador de Primera");
+let jugador3 = new Futbolista("Luis", "Suarez", 39, 80, "Suplente");
+let jugador4 = new Futbolista("Angel", "Di Maria", 37, 80, "Jugador de Primera");
 
 //crear un plantel de futbolistas y que el entrenador lo entrene para salir de viaje !!! Idem masajista
 
@@ -20,10 +20,9 @@ jugador2.entrenamientoCompleto();
 jugador3.covid();
 console.log(jugador4.getEstadoFisico());
 console.log(jugador1.getCategoria());
+
 try {
     jugador4.getEstadoFisico();
-
-}catch(error){
+} catch (error) {
     console.log("error en cero");
-
 }
