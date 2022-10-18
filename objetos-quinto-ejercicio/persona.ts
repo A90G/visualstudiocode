84f9e78ad0f8fdf985a2c44b2f0f7b3a8647c602
irinamarzioni <imarzioni@devartis.com>
2022-10-18 00:05:26 -0300

    correccion ejercicio futbol

--- objetos-quinto-ejercicio/persona.ts
+++ objetos-quinto-ejercicio/persona.ts
@@ -1,32 +1,31 @@
-export class Persona{
-    protected nombre:string;
-    protected apellido:string;
-    protected edad:number;
-    
-    constructor (pNombre:string, pApellido:string, pEdad:number){
-    this.nombre=pNombre;
-    this.apellido=pApellido;
-    this.edad=pEdad;
-    }
+export class Persona {
+   protected nombre: string;
+   protected apellido: string;
+   protected edad: number;
 
- /*cada get con su set para practicar*/
- public getNombre(): string {
-    return this.nombre;
- }
- public setNombre(pNombre): string {
-   return this.nombre = pNombre;
-}
- public getApellido(): string {
-    return this.apellido;
- }
- public setApellido(pApellido): string {
-   return this.apellido = pApellido;
-}
-public getEdad(): number {
-    return this.edad;
- }
- public setEdad(pEdad): number {
-   return this.edad = pEdad;
-}
-    
+   constructor(pNombre: string, pApellido: string, pEdad: number) {
+      this.nombre = pNombre;
+      this.apellido = pApellido;
+      this.edad = pEdad;
+   }
+
+   /*cada get con su set para practicar*/
+   public getNombre(): string {
+      return this.nombre;
+   }
+   public setNombre(pNombre): string {
+      return this.nombre = pNombre;
+   }
+   public getApellido(): string {
+      return this.apellido;
+   }
+   public setApellido(pApellido): string {
+      return this.apellido = pApellido;
+   }
+   public getEdad(): number {
+      return this.edad;
+   }
+   public setEdad(pEdad): number {
+      return this.edad = pEdad;
+   }
 }
